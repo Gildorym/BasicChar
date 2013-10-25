468fdc3dfd12b30d3fe33cd1e2bbf0434e6a4ece
melodichand <melodicbusiness@yahoo.com>
2013-10-25 13:29:05 -0400

    Update CharacterProfession.java

--- src/com/gildorymrp/gildorymclasses/CharacterProfession.java
+++ src/com/gildorymrp/gildorymclasses/CharacterProfession.java
@@ -1,15 +1,9 @@
-/*   */ package com.gildorymrp.gildorymclasses;
-/*   */ 
-/*   */ public enum CharacterProfession
-/*   */ {
-/* 5 */   ALCHEMIST, 
-/* 6 */   COOK, 
-/* 7 */   FARMER, 
-/* 8 */   MINER, 
-/* 9 */   SMITH;
-/*   */ }
+package com.gildorymrp.gildorymclasses;
 
-/* Location:           C:\Users\Administrator.Banditt-PC\Desktop\Coding\Server\plugins\GildorymClasses.jar
- * Qualified Name:     com.gildorymrp.gildorymclasses.CharacterProfession
- * JD-Core Version:    0.6.2
- */
+public enum CharacterProfession{
+  ALCHEMIST, 
+  COOK, 
+  FARMER, 
+  MINER, 
+  SMITH;
+}
