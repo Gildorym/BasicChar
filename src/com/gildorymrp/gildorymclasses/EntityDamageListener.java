74f6944eedcd18af8a86e6061ea3f9761fa4aaae
melodichand <melodicbusiness@yahoo.com>
2013-10-25 13:45:10 -0400

    Update EntityDamageListener.java

--- src/com/gildorymrp/gildorymclasses/EntityDamageListener.java
+++ src/com/gildorymrp/gildorymclasses/EntityDamageListener.java
@@ -1,26 +1,19 @@
-/*    */ package com.gildorymrp.gildorymclasses;
-/*    */ 
-/*    */ import org.bukkit.entity.HumanEntity;
-/*    */ import org.bukkit.entity.Player;
-/*    */ import org.bukkit.event.EventHandler;
-/*    */ import org.bukkit.event.Listener;
-/*    */ import org.bukkit.event.entity.EntityDamageEvent;
-/*    */ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-/*    */ 
-/*    */ public class EntityDamageListener
-/*    */   implements Listener
-/*    */ {
-/*    */   @EventHandler
-/*    */   public void onEntityDamage(EntityDamageEvent event)
-/*    */   {
-/* 14 */     if (((event.getEntity() instanceof Player)) && 
-/* 15 */       ((event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) || (event.getCause() == EntityDamageEvent.DamageCause.PROJECTILE)) && 
-/* 16 */       (((HumanEntity)event.getEntity()).isBlocking()))
-/* 17 */       event.setCancelled(true);
-/*    */   }
-/*    */ }
+package com.gildorymrp.gildorymclasses;
 
-/* Location:           C:\Users\Administrator.Banditt-PC\Desktop\Coding\Server\plugins\GildorymClasses.jar
- * Qualified Name:     com.gildorymrp.gildorymclasses.EntityDamageListener
- * JD-Core Version:    0.6.2
- */
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+public class EntityDamageListener implements Listener{
+
+  @EventHandler
+  public void onEntityDamage(EntityDamageEvent event){
+    if (((event.getEntity() instanceof Player)) && ((event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) || (event.getCause() == EntityDamageEvent.DamageCause.PROJECTILE)) && (((HumanEntity)event.getEntity()).isBlocking())){
+      event.setCancelled(true);
+    }
+  }
+  
+}
